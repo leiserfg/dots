5706a1f3ac0550f0dda4de8187a5525d49a24157
leiserfg <leiserfg@gmail.com>
2019-05-23 12:14:48 +0200

    Remove fish greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.0J1X3l/fish_greeting.fish @ line 2
+# Defined in /tmp/fish.Ae29UP/fish_greeting.fish @ line 2
 function fish_greeting
-	echo "\"If I'm not back in five minutes, just wait longer.\" - Ace Ventura, Ace ventura, Pet Detective"
+	
 end
