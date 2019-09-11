439585f1f9750318a9579fe5d641b1486eb7199a
leiserfg <leiserfg@gmail.com>
2019-09-11 16:57:18 +0200

    Make bspwmrc sh compatible

--- .config/fish/functions/tovp9.fish
+++ .config/fish/functions/tovp9.fish
@@ -1,6 +1,5 @@
+# Defined in /tmp/fish.FI3j7k/tovp9.fish @ line 2
 function tovp9 --argument source out
-	ffmpeg -y -i $source -c:v libvpx-vp9 -pass 1 -b:v 0 -crf 33 -threads 8 -speed 4 -tile-columns 6 -frame-parallel 1 -an -f webm /dev/null
-
-
-    ffmpeg -y -i $source -c:v libvpx-vp9 -pass 2 -b:v 0 -crf 33 -threads 8 -speed 2 -tile-columns 6 -frame-parallel 1 {$out}.webm
+	ffmpeg -y -i $source -c:v libvpx-vp9 -pass 1 -b:v 0 -crf 30 -threads 8 -row-mt 1 -an -f webm /dev/null
+    ffmpeg -y -i $source -pass 2 -c:a libopus  -c:v libvpx-vp9   -b:v 0 -crf 30 -threads 8 -row-mt 1    {$out}.webm
 end
