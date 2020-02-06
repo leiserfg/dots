3ecfd4a2a7e9d216699c7bfbe8d81580cf5c88b9
leiserfg <leiserfg@gmail.com>
2020-02-06 10:00:52 +0100

    Remove function

--- .config/fish/functions/invtk.fish
+++
@@ -1,7 +0,0 @@
-# Defined in /tmp/fish.lwsM9P/invtk.fish @ line 2
-function invtk
-	export INV_TK=(curl -X POST   https://app.inventorum.com/api/api-auth-token/ \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -d '{   "email":"gm+dev3@inventorum.com",   "password":"Epages2018" }'| jq -r .token)
-end
