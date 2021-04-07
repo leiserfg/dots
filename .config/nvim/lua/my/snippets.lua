76b1c56c319cb607d9c19ebe501455bcb2028035
leiserfg <leiserfg@gmail.com>
2021-04-07 09:31:01 +0200

    Add first snippets

--- .config/nvim/lua/my/snippets.lua
+++ .config/nvim/lua/my/snippets.lua
@@ -8,18 +8,39 @@ local c = ls.c
 local d = ls.d
 
 
+math.randomseed(os.time())
+
+local function uuid()
+    local random = math.random
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    local out = template:gsub('[xy]', function (c)
+        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+        return string.format('%x', v)
+    end)
+    return out
+end
+
 local function copy(args, a1, a2)
 	return args[1]
 end
 
+local function sf(trig, body, regexTrig) 
+   local wordTrig = true
+    if regexTrig then
+        wordTrig = false
+    else
+        wordTrig = true
+        regexTrig = false
+    end
+
+    return s({trig = trig, wordTrig = true},
+        {f(body, {}), i(0)}
+     )
+end
+
 ls.snippets = {
 	all = {
-    s({trig = "date", wordTrig = true},
-        {f(function() os.date() end, {})}
-     ),
-
-    s({trig = "date1", wordTrig = true},
-        {f(function() t{os.date()} end, {})}
-     ),
+    sf("date", function() return {os.date("%Y-%m-%d")} end),
+    sf("uuid", function() return {uuid()} end),
  }
 }
