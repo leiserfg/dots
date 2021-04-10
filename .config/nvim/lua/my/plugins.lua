053f453f725dac9ca8883838097f9109771a558a
leiserfg <leiserfg@gmail.com>
2021-04-10 22:10:15 +0200

    Update gitsigns

--- .config/nvim/lua/my/plugins.lua
+++ .config/nvim/lua/my/plugins.lua
@@ -73,7 +73,6 @@ return require('packer').startup(function(use)
 
     use {
         'lewis6991/gitsigns.nvim',
-        branch='yadm',
         requires = {
             'nvim-lua/plenary.nvim'
         },
