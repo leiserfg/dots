8894ee975a3a07118289465140997a4b643b8e86
leiserfg <leiserfg@gmail.com>
2021-04-15 09:12:43 +0200

    Update update function

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.INGs4F/update.fish @ line 2
+# Defined in /tmp/fish.JOIMqk/update.fish @ line 2
 function update
 	yay
-    vim +PackerSync  +qall > /dev/null
+    vim +PackerCompile +PackerSync  +qall > /dev/null
     pipx upgrade-all --include-deps > /dev/null  2>/dev/null
     rustup update > /dev/null 
 end
