f7949cefd7c96d89834dbc347bcaedd34e2ffe03
leiserfg <leiserfg@gmail.com>
2021-04-22 08:22:43 +0200

    Fix pipx update

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.JOIMqk/update.fish @ line 2
+# Defined in /tmp/fish.L6sTwK/update.fish @ line 2
 function update
 	yay
     vim +PackerCompile +PackerSync  +qall > /dev/null
-    pipx upgrade-all --include-deps > /dev/null  2>/dev/null
+    pipx upgrade-all --include-injected > /dev/null  2>/dev/null
     rustup update > /dev/null 
 end
