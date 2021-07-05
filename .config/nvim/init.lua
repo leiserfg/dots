ebc1221b91b2483c4c1f0cd2a05407cbaf24c867
leiserfg <leiserfg@gmail.com>
2021-07-05 13:10:55 +0200

    Fix Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,18 +3,7 @@ vim.cmd [[
 	au VimLeave * lua require'jit.p'.stop()
 ]]
 
--- Entrypoint for my Neovim configuration!
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
-end
 require "my.options"
 require "my.mapping"
--- require "my.pkgs"
 
 vim.cmd [[ au VimEnter * ++once lua require "my.pkgs"]]
