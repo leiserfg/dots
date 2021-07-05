889b9532a63a3a08d6259d5b25409bb23aaeb483
leiserfg <leiserfg@gmail.com>
2021-07-05 13:40:21 +0200

    Fix Update

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,7 +1,7 @@
-# Defined in /tmp/fish.tiUT8w/update.fish @ line 2
+# Defined in /tmp/fish.B12A3G/update.fish @ line 2
 function update
-	yay
-    vim  "+au User PackerComplete qall" +PackerSync
+    yay
+    nvim "+lua require 'my.pkgs'"  "+au User PackerComplete qall" +PackerSync
     pipx upgrade-all --include-injected > /dev/null  2>/dev/null
     rustup update > /dev/null 
 end
