0bd5aa1db912871201b7c50114da788d0a183781
leiserfg <leiserfg@gmail.com>
2021-07-09 10:11:21 +0200

    Some cleanup

--- .config/fish/functions/release_diff.fish
+++ .config/fish/functions/release_diff.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.2wxrHE/release_diff.fish @ line 2
-function release_diff --wraps='git tag --sort=-creatordate|head -n 2|tac|xargs git diff > /tmp/(git tag --sort=-creatordate|head -n 1).diff'
+# Defined in /tmp/fish.KUctYJ/release_diff.fish @ line 2
+function release_diff
   set last  (git tag --sort=-creatordate|head -n 1)
-  git tag --sort=-creatordate|head -n 2|tac|xargs git diff > /tmp/$last.diff $argv; 
-  echo $last
+  git tag --sort=-creatordate|head -n 2|tac|xargs git diff | filterdiff -x"*CHANGES.rst"  -x"*.tf" -x"*.tfvars" -x"*.hcl" "-x*Dockerfile" "-x*.yml" "-x*celery.conf" > /tmp/$last.diff 
+  echo /tmp/$last.diff
 end
