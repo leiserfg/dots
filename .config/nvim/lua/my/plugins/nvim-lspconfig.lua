8574ccd86e7168237fabe42d4f69218835d7f86d
leiserfg <leiserfg@gmail.com>
2021-07-13 10:50:27 +0200

    Fix lsp shortcuts

--- .config/nvim/lua/my/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/my/plugins/nvim-lspconfig.lua
@@ -1,8 +1,24 @@
-local lspconfig = require "lspconfig"
-for _, lsp in ipairs { "pylsp", "gdscript", "vimls", "tsserver", "clangd" } do
-  lspconfig[lsp].setup {}
+local function on_attach()
+   vim.cmd[[
+    nnoremap <buffer> <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
+    nnoremap <buffer> <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
+    nnoremap <buffer> <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
+    nnoremap <buffer> <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+    nnoremap <buffer> <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
+    nnoremap <buffer> <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
+    nnoremap <buffer> <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
+    nnoremap <buffer> <silent> <Leader>= <cmd>lua vim.lsp.buf.formatting()<CR>
+    nnoremap <buffer> <silent> <Leader>q <cmd>lua vim.lsp.diagnostic.set_loclist()<CR>
+    nnoremap <buffer> <silent> [d <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
+    nnoremap <buffer> <silent> ]d <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
+    nnoremap <buffer> <silent> <Leader>a <cmd>lua vim.lsp.buf.code_action()<CR>
+    ]]
 end
 
+local lspconfig = require "lspconfig"
+for _, lsp in ipairs { "pylsp", "gdscript", "vimls", "tsserver", "clangd" } do
+  lspconfig[lsp].setup {on_attach=on_attach}
+end
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
@@ -10,6 +26,7 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 require'lspconfig'.sumneko_lua.setup {
   cmd = {"/usr/bin/lua-language-server"};
+  on_attach=on_attach;
   settings = {
     Lua = {
       runtime = {
@@ -35,31 +52,11 @@ require'lspconfig'.sumneko_lua.setup {
 }
 
 vim.cmd [[
-augroup lsp
-autocmd!
-augroup END
-
-function! LspSetup()
-    if empty(luaeval('vim.inspect(vim.lsp.buf_get_clients())'))
-        return
-    end
-    nnoremap <buffer> <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
-    nnoremap <buffer> <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
-    nnoremap <buffer> <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
-    nnoremap <buffer> <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-    nnoremap <buffer> <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
-    nnoremap <buffer> <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
-    nnoremap <buffer> <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
-    nnoremap <buffer> <silent> <Leader>= <cmd>lua vim.lsp.buf.formatting()<CR>
-    nnoremap <buffer> <silent> <Leader>q <cmd>lua vim.lsp.diagnostic.set_loclist()<CR>
-    nnoremap <buffer> <silent> [d <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
-    nnoremap <buffer> <silent> ]d <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
-    nnoremap <buffer> <silent> <Leader>a <cmd>lua vim.lsp.buf.code_action()<CR>
-endfunction
-au lsp BufEnter * call LspSetup()
-
-sign define LspDiagnosticsSignError text=🩸 linehl= numhl=
-sign define LspDiagnosticsSignWarning text=🔸 linehl= numhl=
-sign define LspDiagnosticsSignInformation text=🔹 linehl= numhl=
-sign define LspDiagnosticsSignHint text=👉 linehl= numhl=
+  sign define LspDiagnosticsSignError text=🩸 linehl= numhl=
+  sign define LspDiagnosticsSignWarning text=🔸 linehl= numhl=
+  sign define LspDiagnosticsSignInformation text=🔹 linehl= numhl=
+  sign define LspDiagnosticsSignHint text=👉 linehl= numhl=
 ]]
+
+
+
