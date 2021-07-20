b91821ad5f14c3728a81e6beea457a06961f67af
leiserfg <leiserfg@gmail.com>
2021-07-20 12:42:25 +0200

    Update release diff

--- .config/fish/functions/release_diff.fish
+++ .config/fish/functions/release_diff.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.KUctYJ/release_diff.fish @ line 2
 function release_diff
   set last  (git tag --sort=-creatordate|head -n 1)
-  git tag --sort=-creatordate|head -n 2|tac|xargs git diff | filterdiff -x"*CHANGES.rst"  -x"*.tf" -x"*.tfvars" -x"*.hcl" "-x*Dockerfile" "-x*.yml" "-x*celery.conf" > /tmp/$last.diff 
+  git tag --sort=-creatordate|head -n 2|tac|xargs -n2 sh -c 'echo git diff --binary $1 $2 \':!*CHANGES.rst\' \':!/*.tf\'   \':!/*.tfvars\'  \':!/*.hcl\'  \':!/*Dockerfile\' \':!/*.yml\'  \':!/*celery.conf\' ' sh > /tmp/$last.diff
+  # "| filterdiff -x"*CHANGES.rst"  -x"*.tf" -x"*.tfvars" -x"*.hcl" "-x*Dockerfile" "-x*.yml" "-x*celery.conf" > /tmp/$last.diff 
   echo /tmp/$last.diff
 end
