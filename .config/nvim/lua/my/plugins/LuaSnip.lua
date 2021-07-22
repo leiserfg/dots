d8e492bf7fd718392c1c267c33c7f399c2efd23b
leiserfg <leiserfg@gmail.com>
2021-07-22 23:09:22 +0200

    Thu Jul 22 11:09:22 PM CEST 2021

--- .config/nvim/lua/my/plugins/LuaSnip.lua
+++ .config/nvim/lua/my/plugins/LuaSnip.lua
@@ -2,9 +2,18 @@ require "my/snippets"
 local ls = require "luasnip"
 ls.config.set_config { updateevents = "TextChanged,TextChangedI" }
 vim.cmd [[
-  imap <silent><expr> <c-j> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-j>' "
-  imap <silent><expr> <c-k>  luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev': '<c-k>'"
-  imap <silent><expr> <c-e> luasnip#choice_active() ? '<Plug>luasnip-next-choice': '<c-e>'"
-  snoremap <silent> <c-j> <cmd>lua require'luasnip'.jump(1)<Cr>"
-  snoremap <silent> <c-k> <cmd>lua require'luasnip'.jump(-1)<Cr>"
+  imap <silent><expr> <c-j> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-j>'
+  imap <silent><expr> <c-k>  luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev': '<c-k>'
+  imap <silent><expr> <c-e> luasnip#choice_active() ? '<Plug>luasnip-next-choice': '<c-e>'
+  snoremap <silent> <c-j> <cmd>lua require'luasnip'.jump(1)<Cr>
+  snoremap <silent> <c-k> <cmd>lua require'luasnip'.jump(-1)<Cr>
 ]]
+
+
+
+
+
+
+
+
+
