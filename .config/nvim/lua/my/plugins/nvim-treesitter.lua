54a99cd6f5b229f239e36c704aab87e134da490e
leiserfg <leiserfg@gmail.com>
2021-08-04 16:57:01 +0200

    Mi 4. Aug 16:57:01 CEST 2021

--- .config/nvim/lua/my/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/my/plugins/nvim-treesitter.lua
@@ -3,7 +3,6 @@ ts.setup {
   ensure_installed = "all",
   highlight = { disable = { "markdown" }, enable = true },
   playground = { enable = true, persist_queries = false },
-  indent = {enable=true},
   textobjects = {
     enable = true,
     keymaps = {
