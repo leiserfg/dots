76a2dbcdb6dc47a4473b65586eef5a86c379692d
leiserfg <leiserfg@gmail.com>
2021-11-08 10:37:57 +0100

    Mon Nov  8 10:37:57 AM CET 2021

--- .config/fish/functions/codi.fish
+++ .config/fish/functions/codi.fish
@@ -1,10 +1,7 @@
-# Defined in /tmp/fish.n32CYr/codi.fish @ line 2
 function codi --argument lang
   set -q lang[1] || set lang python
-  echo $lang
   nvim -c \
-    "let g:startify_disable_at_vimenter = 1 |\
-    set bt=nofile ls=0 noru nonu nornu |\
+    "set bt=nofile ls=0 noru nonu nornu |\
     hi ColorColumn ctermbg=NONE |\
     hi VertSplit ctermbg=NONE |\
     hi NonText ctermfg=0 |\
