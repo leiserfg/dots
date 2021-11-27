b7b5b3d94b239a5069ee21e292c2f6e2ab06d2bb
leiserfg <leiserfg@gmail.com>
2021-11-27 19:11:23 +0100

    Sat Nov 27 07:11:23 PM CET 2021

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ export MANPAGER='nvim +Man!'
 export VIRTUAL_ENV_DISABLE_PROMPT="no prompt"
 # set PATH "/usr/local/opt/python/libexec/bin" $HOME/.local/bin $PATH
 set PATH $HOME/.local/bin $PATH
-
+set PATH $HOME/.nimble/bin $PATH
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
