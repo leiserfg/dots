01b0c77e8241eab9f0334a4899c1cf7889d707a4
leiserfg <leiserfg@gmail.com>
2022-02-04 10:28:18 +0100

    Fr 4. Feb 10:28:18 CET 2022

--- .config/fish/functions/release_diff.fish
+++ .config/fish/functions/release_diff.fish
@@ -1,6 +1,6 @@
 function release_diff
   set last  (git tag --sort=-creatordate|head -n 1)
-  git tag --sort=-creatordate|head -n 2|tac|xargs -n2 sh -c 'git diff --binary $1 $2 \':!*CHANGES.rst\' \':!/*.tf\'  \':!/*.ini\'  \':!/*.tfvars\'  \':!/*.hcl\' \':!/*Dockerfile\' \':!/*.yml\'  \':!/*celery.conf\'  \':!/*settings.py\'  ' sh > /tmp/$last.diff
+  git tag --sort=-creatordate|head -n 2|tac|xargs -n2 sh -c 'git diff --binary $1 $2 \':!*CHANGES.rst\' \':!/*.tf\'  \':!/*.ini\'  \':!/*.tfvars\'  \':!/*.hcl\' \':!/*Dockerfile\' \':!/*.yml\'  \':!/*celery.conf\'  \':!/*settings.py\'  \':!docker\'    ' sh > /tmp/$last.diff
   # "| filterdiff -x"*CHANGES.rst"  -x"*.tf" -x"*.tfvars" -x"*.hcl" "-x*Dockerfile" "-x*.yml" "-x*celery.conf" > /tmp/$last.diff 
   echo /tmp/$last.diff
 end
