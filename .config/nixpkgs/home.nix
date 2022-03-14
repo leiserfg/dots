24d6888d75ade467c5baef870288b0912c8e3f64
leiserfg <leiserfg@gmail.com>
2022-03-14 11:31:33 +0100

    Mo 14. Mär 11:31:33 CET 2022

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -4,7 +4,7 @@ let nixGL = (pkgs.callPackage "${builtins.fetchTarball {
       url = https://github.com/guibou/nixGL/archive/17c1ec63b969472555514533569004e5f31a921f.tar.gz;
       sha256 = "0yh8zq746djazjvlspgyy1hvppaynbqrdqpgk447iygkpkp3f5qr";
     }}/nixGL.nix"
-  { }).nixGLDefault;
+  { }).nixGL;
 in
 {
   # Home Manager needs a bit of information about you and the
@@ -22,9 +22,24 @@ in
   # changes in each release.
   home.stateVersion = "22.05";
 
-  home.sessionVariables = {
+  home.sessionVariables = with pkgs; let
+        mesa-drivers = [ mesa.drivers ]
+          ++ lib.optional true pkgsi686Linux.mesa.drivers;
+        intel-driver = [ intel-media-driver vaapiIntel ]
+          # Note: intel-media-driver is disabled for i686 until https://github.com/NixOS/nixpkgs/issues/140471 is fixed
+          ++ lib.optionals true [ /* pkgsi686Linux.intel-media-driver */ driversi686Linux.vaapiIntel ];
+        libvdpau = [ libvdpau-va-gl ]
+          ++ lib.optional true pkgsi686Linux.libvdpau-va-gl;
+        glxindirect = runCommand "mesa_glxindirect" { } (''
+          mkdir -p $out/lib
+          ln -s ${mesa.drivers}/lib/libGLX_mesa.so.0 $out/lib/libGLX_indirect.so.0
+        '');
+      in {
    # VK_ICD_FILENAMES="${nvidiaLibsOnly}/share/vulkan/icd.d/nvidia_icd.x86_64.json"
-  };
+      LD_LIBRARY_PATH="${lib.makeLibraryPath mesa-drivers}:${lib.makeSearchPathOutput "lib" "lib/vdpau" libvdpau}:${glxindirect}/lib:${lib.makeLibraryPath [libglvnd]}";
+      LIBGL_DRIVERS_PATH="${lib.makeSearchPathOutput "lib" "lib/dri" mesa-drivers}";
+      LIBVA_DRIVERS_PATH="${lib.makeSearchPathOutput "out" "lib/dri" intel-driver}";
+};
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
       (import (
@@ -44,11 +59,11 @@ in
   programs.mpv = {
       enable = true;
       # scripts = [pkgs.mpvScripts.mpris];
-      package =
-          pkgs.writeShellScriptBin "mpv" ''
-          #!/bin/sh
-          ${nixGL}/bin/nixGL ${pkgs.mpv}/bin/mpv "$@"
-          '';
+      # package =
+      #     pkgs.writeShellScriptBin "mpv" ''
+      #     #!/bin/sh
+      #     ${nixGL}/bin/nixGL ${pkgs.mpv}/bin/mpv "$@"
+      #     '';
   };
 
 
