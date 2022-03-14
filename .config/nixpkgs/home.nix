822ca2b1be61bae418182a2ce380566c95b05c77
leiserfg <leiserfg@gmail.com>
2022-03-14 23:57:17 +0100

    Mon Mar 14 11:57:17 PM CET 2022

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
-
 {
   targets.genericLinux.enable = true;  # I don't use NixOS nor MacOS
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "leiserfg";
@@ -18,32 +16,26 @@
   # changes in each release.
   home.stateVersion = "22.05";
 
+ nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+    (builtins.getFlake github:edolstra/nix-warez?dir=blender).overlay
+  ];
+
+
   home.sessionVariables = let
   hardware = pkgs.callPackage ./hardware.nix {};
   in
     if hardware.productName == "MS-7A38" then  hardware.nvidiaVars else hardware.intelVars;
 
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [
-      (import (
-               builtins.fetchTarball {
-               url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-               }))
-  ];
 
  home.packages = with pkgs; [
     neovim-nightly
     pcmanfm
     krita
-    (writeScriptBin "vim" ''
-     exec ${ neovim-nightly }/bin/nvim "$@"
-     '')
-    (writeScriptBin "vi" ''
-     exec ${ neovim-nightly }/bin/nvim "$@"
-     '')
-    (writeScriptBin "vimdiff" ''
-     exec ${ neovim-nightly }/bin/nvim -d "$@"
-     '')
+    blender_3_1
  ];
 
   # Let Home Manager install and manage itself.
