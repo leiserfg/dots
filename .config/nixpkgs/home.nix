068f4f7dc3cf51015ab4f5e3a1b377a2eae1dc6f
leiserfg <leiserfg@gmail.com>
2022-03-14 10:52:38 +0100

    Mon Mar 14 10:52:38 AM CET 2022

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 
+let nixGL = (pkgs.callPackage "${builtins.fetchTarball {
+      url = https://github.com/guibou/nixGL/archive/17c1ec63b969472555514533569004e5f31a921f.tar.gz;
+      sha256 = "0yh8zq746djazjvlspgyy1hvppaynbqrdqpgk447iygkpkp3f5qr";
+    }}/nixGL.nix"
+  { }).nixGLDefault;
+in
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -19,6 +25,7 @@
   home.sessionVariables = {
    # VK_ICD_FILENAMES="${nvidiaLibsOnly}/share/vulkan/icd.d/nvidia_icd.x86_64.json"
   };
+  nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
       (import (
                builtins.fetchTarball {
@@ -32,10 +39,16 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.fzf.enable = true;
   programs.lf.enable = true;
   programs.mpv = {
       enable = true;
-      scripts = [pkgs.mpvScripts.mpris];
+      # scripts = [pkgs.mpvScripts.mpris];
+      package =
+          pkgs.writeShellScriptBin "mpv" ''
+          #!/bin/sh
+          ${nixGL}/bin/nixGL ${pkgs.mpv}/bin/mpv "$@"
+          '';
   };
 
 
