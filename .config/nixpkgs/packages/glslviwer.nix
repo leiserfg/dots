6fe6e7e8dd545d134c1191194a6d9cc21d0b41ea
leiserfg <leiserfg@gmail.com>
2022-03-16 16:57:14 +0100

    Wed Mar 16 04:57:14 PM CET 2022

---
+++ .config/nixpkgs/packages/glslviwer.nix
@@ -0,0 +1,31 @@
+{
+    stdenv, cmake, ninja, lib,
+    libX11, libXrandr, libXinerama, libXcursor, libXi, libXext, libGLU, ffmpeg
+}:
+
+stdenv.mkDerivation rec {
+  pname = "glslViewer";
+  version = "2.0.4";
+  src = fetchFromGitHub {
+      owner = "patriciogonzalezvivo";
+      repo = pname;
+      fetchSubmodules = true;
+      rev = version;
+      sha256 = "sha256-Jrj8WlKrkPpmME50oVcZLQZs0bqbODAYibLJGPF7ock=";
+  };
+  nativeBuildInputs = [cmake ninja];
+  buildInputs = [libX11 libXrandr libXinerama libXcursor libXi libXext libGLU ffmpeg];
+
+  cmakeFlags = [
+      "-DCMAKE_BUILD_TYPE='Release'"
+      "-GNinja"
+  ];
+
+  meta = with lib; {
+      description = "Live GLSL coding renderer";
+      homepage = "http://patriciogonzalezvivo.com/2015/glslViewer/";
+      license = licenses.bsd3;
+      platforms = platforms.linux;
+      # maintainers = [ maintainers.hodapp ];
+  };
+}
