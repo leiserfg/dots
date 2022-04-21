eaaf75aac8065bfce7dce99c81480748aca69592
leiserfg <leiserfg@gmail.com>
2022-04-21 23:47:33 +0200

    Thu Apr 21 11:47:33 PM CEST 2022

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -72,10 +72,14 @@ keys = [
         [mod],
         "g",
         lazy.spawn(
-            """
-        sh -c "lutris -lo 2>/dev/null |cut -f 1,2 -d '|' | rofi -dmenu -i|
-        cut -f 1 -d '|'  | xargs -I__ lutris lutris:rungameid/__"
-    """
+        r"""
+            sh -c "ls ~/Games/*/start.sh  --quoting-style=escape \
+            |sed -e 's/.*Games\///' -e 's/\/start.sh.*//' \
+            |rofi -dmenu -i  \
+            |xargs  -d '\n'  -I__  sh -c ~/Games/__/start.sh"
+        """
+        # sh -c "lutris -lo 2>/dev/null |cut -f 1,2 -d '|' | rofi -dmenu -i|
+        # cut -f 1 -d '|'  | xargs -I__ lutris lutris:rungameid/__"
         ),
         desc="Games List",
     ),
