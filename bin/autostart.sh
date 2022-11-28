3deb62a4025f737180437324d4994fbff4c2db64
leiserfg <leiserfg@gmail.com>
2022-11-28 10:44:43 +0100

    Mon 28 Nov 2022 10:44:43 AM CET

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -11,14 +11,6 @@ run_disowned () {
 
 xset -b
 pkill -9 picom; picom -b
-autorandr --change
 
-xset s 300 600
-run_once xsettingsd
-run_once light-locker --lock-on-lid
-run_once /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-run_once nm-applet
-run_once pasystray
-run_once unclutter -b #hide the cursor when not moving it
-
+run_once ~/.nix-profile/libexec/polkit-gnome-authentication-agent-1
 
