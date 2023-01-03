4dc9ef93b043f04cf28bda34225219e7f9f59bbb
leiserfg <leiserfg@gmail.com>
2023-01-03 23:42:34 +0100

    Tue Jan  3 11:42:34 PM CET 2023

--- .config/nvim/lua/my/plugins/dial.lua
+++ .config/nvim/lua/my/plugins/dial.lua
@@ -1,9 +1,31 @@
 local dm = require("dial.map")
-local setkm = vim.keymap.set
 local augend = require("dial.augend")
-setkm("n", "<c-a>", dm.inc_normal, {noremap = true})
-setkm("n", "<c-x>", dm.dec_normal, {noremap = true})
-setkm("n", "<c-a>", dm.inc_visual, {noremap = true})
-setkm("n", "<c-x>", dm.dec_visual, {noremap = true})
-setkm("n", "<c-a>", dm.inc_gvisual, {noremap = true})
-return setkm("n", "<c-x>", dm.dec_gvisual, {noremap = true})
+
+local setkm = vim.keymap.set
+
+setkm("n", "<C-A>", dm.inc_normal(), { noremap = true })
+setkm("n", "<C-X>", dm.dec_normal(), { noremap = true })
+setkm("v", "<C-A>", dm.inc_visual(), { noremap = true })
+setkm("v", "<C-X>", dm.dec_visual(), { noremap = true })
+setkm("v", "g<C-A>", dm.inc_gvisual(), { noremap = true })
+setkm("v", "g<C-X>", dm.dec_gvisual(), { noremap = true })
+
+local function words(vals)
+    return augend.constant.new {
+        elements = vals,
+        word = true,
+        cyclic = true,
+    }
+end
+
+require("dial.config").augends:register_group({
+    default = {
+        augend.date.alias["%Y-%m-%d"],
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.constant.alias.bool,
+        augend.semver.alias.semver,
+        words { "staging", "production" },
+        words { "False", "True" },
+    },
+})
