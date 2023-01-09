08a1ca83ccf31c9511cc34a92ac0010434f44af6
leiserfg <leiserfg@gmail.com>
2023-01-09 10:47:00 +0100

    Mon Jan  9 10:47:00 AM CET 2023

--- .config/nvim/lua/my/plugins/init.lua
+++ .config/nvim/lua/my/plugins/init.lua
@@ -1,43 +1,43 @@
 return {
-    { "tpope/vim-unimpaired", event = "BufRead" },
-    { "numToStr/Comment.nvim", config = true, keys = { { "gc", mode = { "n", "v" } } } },
-    { "vim-scripts/ReplaceWithRegister", keys = { { "gr", mode = { "n", "v" } } } },
-    "tpope/vim-repeat",
-    { "Olical/vim-enmasse", cmd = "EnMasse" },
-    "tpope/vim-eunuch",
-    { "Vimjas/vim-python-pep8-indent", ft = "python" },
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = true,
-    },
-    "direnv/direnv.vim",
-    {
-        "kylechui/nvim-surround",
-        config = true,
-    },
-    -- { "AndrewRadev/splitjoin.vim",    keys = { "gS", "gJ" } },
+  { "tpope/vim-unimpaired", event = "BufRead" },
+  { "numToStr/Comment.nvim", config = true, keys = { { "gc", mode = { "n", "v" } } } },
+  { "vim-scripts/ReplaceWithRegister", keys = { { "gr", mode = { "n", "v" } } } },
+  "tpope/vim-repeat",
+  { "Olical/vim-enmasse", cmd = "EnMasse" },
+  "tpope/vim-eunuch",
+  { "Vimjas/vim-python-pep8-indent", ft = "python" },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = true,
+  },
+  "direnv/direnv.vim",
+  {
+    "kylechui/nvim-surround",
+    config = true,
+  },
+  -- { "AndrewRadev/splitjoin.vim",    keys = { "gS", "gJ" } },
 
-    {
-        "lambdalisue/suda.vim",
-        init = function()
-            vim.g.suda_smart_edit = 1
-        end,
-    },
+  {
+    "lambdalisue/suda.vim",
+    init = function()
+      vim.g.suda_smart_edit = 1
+    end,
+  },
 
-    {
-        "dhruvasagar/vim-table-mode",
-        ft = { "markdown" },
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("kanagawa").setup()
-            vim.cmd "colorscheme kanagawa"
-        end,
-    },
-    { "nanotee/zoxide.vim", cmd = "Z", },
-    { "stevearc/oil.nvim", config = true },
-    { "tweekmonster/startuptime.vim", cmd = "StartupTime", },
+  {
+    "dhruvasagar/vim-table-mode",
+    ft = { "markdown" },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup()
+      vim.cmd "colorscheme kanagawa"
+    end,
+  },
+  { "nanotee/zoxide.vim", cmd = "Z" },
+  { "stevearc/oil.nvim", config = true },
+  { "tweekmonster/startuptime.vim", cmd = "StartupTime" },
 }
