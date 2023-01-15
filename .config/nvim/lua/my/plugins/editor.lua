2230b30e29e4e4374d25cc685732762dca0f9fa1
leiserfg <leiserfg@gmail.com>
2023-01-15 21:40:25 +0100

    Sun Jan 15 09:40:25 PM CET 2023

--- .config/nvim/lua/my/plugins/editor.lua
+++ .config/nvim/lua/my/plugins/editor.lua
@@ -7,12 +7,25 @@ return {
 
       local setkm = vim.keymap.set
 
-      setkm("n", "<C-A>", dm.inc_normal(), { noremap = true })
-      setkm("n", "<C-X>", dm.dec_normal(), { noremap = true })
-      setkm("v", "<C-A>", dm.inc_visual(), { noremap = true })
-      setkm("v", "<C-X>", dm.dec_visual(), { noremap = true })
-      setkm("v", "g<C-A>", dm.inc_gvisual(), { noremap = true })
-      setkm("v", "g<C-X>", dm.dec_gvisual(), { noremap = true })
+function _G._group_from_ft()
+  local ft = vim.o.filetype
+  if require("dial.config").augends.group[ft] then
+    return ft
+  else
+    return "default"
+  end
+end
+
+
+local  group_from_ft = [[".._group_from_ft().."]]
+
+
+      setkm("n", "<C-A>", dm.inc_normal(group_from_ft), { noremap = true })
+      setkm("n", "<C-X>", dm.dec_normal(group_from_ft), { noremap = true })
+      setkm("v", "<C-A>", dm.inc_visual(group_from_ft), { noremap = true })
+      setkm("v", "<C-X>", dm.dec_visual(group_from_ft), { noremap = true })
+      setkm("v", "g<C-A>", dm.inc_gvisual(group_from_ft), { noremap = true })
+      setkm("v", "g<C-X>", dm.dec_gvisual(group_from_ft), { noremap = true })
 
       local function words(vals)
         return augend.constant.new {
@@ -30,8 +43,10 @@ return {
           augend.constant.alias.bool,
           augend.semver.alias.semver,
           words { "staging", "production" },
-          words { "False", "True" },
         },
+        python = {
+          words { "False", "True" },
+        }
       }
     end,
   },
