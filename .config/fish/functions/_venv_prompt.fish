c1120cda09ccb1952696e3f288ecb677f860e06c
leiserfg <leiserfg@gmail.com>
2023-01-15 17:15:28 +0100

    Sun Jan 15 05:15:28 PM CET 2023

--- .config/fish/functions/_venv_prompt.fish
+++ .config/fish/functions/_venv_prompt.fish
@@ -1,6 +1,6 @@
 # Defined in /tmp/fish.896aQX/_venv_prompt.fish @ line 2
 function _venv_prompt --description 'virtualenv segment'
-	if test $VIRTUAL_ENV
-       printf (set_color normal)'['(set_color blue)(basename $VIRTUAL_ENV|sed 's/python-//')(set_color normal)']'
+  if test $VIRTUAL_ENV
+       printf (set_color normal)'['(set_color blue)(basename $VIRTUAL_ENV|sed 's/python-//'|string shorten --max 10)(set_color normal)']'
   end
 end
