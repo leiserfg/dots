ce0aa654875d789287327c53d0235c6852ab9d81
leiserfg <leiserfg@gmail.com>
2023-02-20 09:47:36 +0100

    Mon Feb 20 09:47:36 AM CET 2023

--- .config/nvim/lua/my/plugins/LuaSnip.lua
+++ .config/nvim/lua/my/plugins/LuaSnip.lua
@@ -1,7 +1,7 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    -- dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
       require "my/snippets"
       local ls = require "luasnip"
