e0e783cd3f5d3ebc6277474948244c98389598b0
leiserfg <leiserfg@gmail.com>
2023-08-28 10:24:12 +0200

    Mon Aug 28 10:24:11 AM CEST 2023

--- .config/nvim/lua/my/plugins/minis.lua
+++ .config/nvim/lua/my/plugins/minis.lua
@@ -1,17 +1,7 @@
 return {
-  -- { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
   {
     "echasnovski/mini.nvim",
     event = "VeryLazy",
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          -- no need to load the plugin, since we only need its queries
-          require("lazy.core.loader").disable_rtp_plugin "nvim-treesitter-textobjects"
-        end,
-      },
-    },
     config = function()
       for _, mini in ipairs { "jump", "align", "move", "splitjoin" } do
         require(("mini.%s"):format(mini)).setup {}
