55c977ac6f0916461a61f82fe9f4dd9296d18d82
leiserfg <leiserfg@gmail.com>
2025-01-08 10:05:59 +0100

    Wed Jan  8 10:05:59 AM CET 2025

--- .config/nvim/lua/my/plugins/cmp.lua
+++ .config/nvim/lua/my/plugins/cmp.lua
@@ -11,18 +11,7 @@ return {
 
     opts = {
       snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
+        preset = "luasnip",
       },
 
       keymap = {
@@ -30,12 +19,8 @@ return {
         ["<C-e>"] = { "hide", "fallback" },
         ["<CR>"] = { "accept", "fallback" },
 
-        ["<S-Tab>"] = { "select_prev", "fallback" },
         ["<Tab>"] = { "select_next", "fallback" },
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback" },
-        ["<C-n>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
 
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
@@ -45,12 +30,8 @@ return {
           ["<C-e>"] = { "hide", "fallback" },
           ["<CR>"] = { "accept", "fallback" },
 
-          ["<S-Tab>"] = { "select_prev", "fallback" },
           ["<Tab>"] = { "select_next", "fallback" },
-          ["<Up>"] = { "select_prev", "fallback" },
-          ["<Down>"] = { "select_next", "fallback" },
-          ["<C-p>"] = { "select_prev", "fallback" },
-          ["<C-n>"] = { "select_next", "fallback" },
+          ["<S-Tab>"] = { "select_prev", "fallback" },
         },
       },
 
@@ -59,9 +40,11 @@ return {
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
         -- Don't autoselect in cmd
         list = {
-          selection = function(ctx)
-            return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-          end,
+          selection = {
+            preselect = function(ctx)
+              return ctx.mode ~= "cmdline"
+            end,
+          },
         },
       },
 
@@ -70,7 +53,7 @@ return {
           "lazydev",
           "lsp",
           "path",
-          "luasnip",
+          "snippets",
           "buffer",
         },
         providers = {
