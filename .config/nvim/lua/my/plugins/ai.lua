824f3d0086be94a0934348c4a57ea6dad8fd511d
leiserfg <leiserfg@gmail.com>
2025-02-11 10:21:53 +0100

    Tue Feb 11 10:21:53 AM CET 2025

--- .config/nvim/lua/my/plugins/ai.lua
+++ .config/nvim/lua/my/plugins/ai.lua
@@ -6,9 +6,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   opts = {
-    opts = {
-      log_level = "TRACE", -- TRACE|DEBUG|ERROR|INFO
-    },
     strategies = {
       chat = {
         adapter = "gemini",
