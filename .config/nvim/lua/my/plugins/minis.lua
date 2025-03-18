c172899a483e5d226e5cb650c569ee4866d31dc4
leiserfg <leiserfg@gmail.com>
2025-03-18 16:32:22 +0100

    Tue Mar 18 04:32:22 PM CET 2025

--- .config/nvim/lua/my/plugins/minis.lua
+++ .config/nvim/lua/my/plugins/minis.lua
@@ -74,6 +74,45 @@ return {
       require("mini.bracketed").setup {
         comment = { suffix = "k" }, -- I use c for changes as diffmode does by default
       }
+      local miniclue = require "mini.clue"
+      miniclue.setup {
+        triggers = {
+          -- Leader triggers
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
+
+          -- `g` key
+          { mode = "n", keys = "g" },
+          { mode = "x", keys = "g" },
+
+          -- Marks
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+
+          -- Registers
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          -- Window commands
+          { mode = "n", keys = "<C-w>" },
+
+          -- `z` key
+          { mode = "n", keys = "z" },
+          { mode = "x", keys = "z" },
+        },
+
+        clues = {
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+      }
     end,
   },
 }
