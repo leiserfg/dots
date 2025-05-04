c10c3c92bb41c44e82c3ced03e0b9fbb039c88a6
leiserfg <leiserfg@gmail.com>
2025-05-04 17:59:13 +0200

    Sun May  4 05:59:13 PM CEST 2025

--- .config/nvim/lua/my/plugins/init.lua
+++ .config/nvim/lua/my/plugins/init.lua
@@ -33,7 +33,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      require("gruvbox").setup { transparent_mode=true, contrast = "hard" }
+      require("gruvbox").setup { transparent_mode = true, contrast = "hard" }
       vim.cmd.colorscheme "gruvbox"
     end,
   },
