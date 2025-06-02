146c47072287aa5eda43af1d29a121ad4e4e76f8
leiserfg <leiserfg@gmail.com>
2025-06-02 09:46:30 +0200

    Mon Jun  2 09:46:30 AM CEST 2025

--- .config/nvim/lua/my/plugins/init.lua
+++ .config/nvim/lua/my/plugins/init.lua
@@ -26,16 +26,18 @@ return {
     ft = { "markdown" },
   },
   {
-    -- "EdenEast/nightfox.nvim",
-    "ellisonleao/gruvbox.nvim",
+    "maxmx03/solarized.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("gruvbox").setup { transparent_mode = true, contrast = "hard" }
-      vim.cmd.colorscheme "gruvbox"
+    ---@type solarized.config
+    opts = { transparent = { enabled = true } },
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = "light"
+      require("solarized").setup(opts)
+      vim.cmd.colorscheme "solarized"
     end,
   },
-
   { "nanotee/zoxide.vim", cmd = "Z" },
   -- { "stevearc/oil.nvim", config = true },
   {
